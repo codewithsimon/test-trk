6096bcc159055efa8597e29a9b1fb050f68dc87a
Stephen SIMON <68376360+codewithsimon@users.noreply.github.com>
2025-10-13 13:20:46 +0530

    Updated Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -36,7 +36,7 @@ const Timeline = () => {
       date: 'October 25',
       title: 'Winner Announcement 🏆',
       description: 'Pre-recorded presentations from selected teams are played at the BGI Conference Istanbul, followed by the winner reveal,',
-      icon: Globe,
+      icon: Trophy,
     },    
   ];
 
