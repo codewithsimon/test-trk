2bc876d801857859aabb27f116edab4944dca518
Stephen SIMON <68376360+codewithsimon@users.noreply.github.com>
2025-10-15 11:09:44 +0530

    Updated Schedule.tsx

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -126,7 +126,7 @@ const Schedule = () => {
                     rel="noopener noreferrer"
                     className="mt-4 block w-full px-4 py-2 bg-gradient-to-r from-[#14a19f] to-[#0d7775] text-white text-center rounded-lg font-semibold hover:scale-105 shadow-lg hover:shadow-[0_0_30px_rgba(20,161,159,0.5)] transition-all duration-300"
                   >
-                    Join Kickoff Session
+                    Watch the Recording
                   </a>
                 )}
               </div>
