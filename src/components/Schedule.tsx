db989919922a198f73b8da2312ba05edbb7bf580
Stephen SIMON <68376360+codewithsimon@users.noreply.github.com>
2025-10-15 11:06:35 +0530

    Updated Schedule.tsx

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -121,7 +121,7 @@ const Schedule = () => {
                 })}
                 {day.day === 'Oct 14' && (
                   <a
-                    href="https://us06web.zoom.us/j/83342532145?pwd=snxNimEtPW316Yb7j6hqW36ZZqrx7w.1"
+                    href="https://us06web.zoom.us/rec/share/sY-_H5Gn2MakH1-Jzs6FflLyP92gn71zRgq5lIGAl5SBpnbpOL9eip9k2SNVvk4_.CGZESAyqjbKexx3R"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mt-4 block w-full px-4 py-2 bg-gradient-to-r from-[#14a19f] to-[#0d7775] text-white text-center rounded-lg font-semibold hover:scale-105 shadow-lg hover:shadow-[0_0_30px_rgba(20,161,159,0.5)] transition-all duration-300"
